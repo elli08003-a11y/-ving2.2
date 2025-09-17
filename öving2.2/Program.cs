3be0df6735b443959dd263edc34a2b135f3f8cbe
elli08003 <elli08003@E-PW04SM2Y>
2025-09-17 13:09:32 +0200

    Add project files.

---
+++ öving2.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+namespace Övning2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Elin hoppade längre en Alma. Hur långt hoppade Elin i meter");
+            string Elin = Console.ReadLine();
+            Console.WriteLine("Hur långt hoppade Alma i meter");
+            string Alma = Console.ReadLine();
+            Double Elinnummer = Convert.ToDouble(Elin);
+            Double Almanummer = Convert.ToDouble(Alma);
+            Double skilnad = (Elinnummer - Almanummer);
+            Console.WriteLine("Elin hoppade " + skilnad + " meter längre en Alma");
+        }
+    }
+}
